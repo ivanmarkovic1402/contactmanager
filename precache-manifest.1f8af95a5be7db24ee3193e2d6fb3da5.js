e924182d05af14520c76c998536687146b1bef74
ivanmarkovic1402 <ivan.markovic1402@gmail.com>
2020-06-10 13:19:31 +0200

    Updates

--- precache-manifest.1f8af95a5be7db24ee3193e2d6fb3da5.js
+++ precache-manifest.1f8af95a5be7db24ee3193e2d6fb3da5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac2003255a16e15d3941c55bf201e7f0",
+    "revision": "eca3e8c17b9214c47903cc7c7bf47802",
     "url": "/contactmanager/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "26d13e3bf761ce961103",
+    "revision": "0dc6434215fa086b31ef",
     "url": "/contactmanager/static/css/main.36497136.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/contactmanager/static/js/2.a5911414.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "26d13e3bf761ce961103",
-    "url": "/contactmanager/static/js/main.89fb3132.chunk.js"
+    "revision": "0dc6434215fa086b31ef",
+    "url": "/contactmanager/static/js/main.fc7880eb.chunk.js"
   },
   {
     "revision": "31aa565394677d9739a4",
