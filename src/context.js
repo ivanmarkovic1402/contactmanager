b2970354ed11df62570aacd64fc358b608479d08
ivanmarkovic1402 <ivan.markovic1402@gmail.com>
2020-06-10 13:19:02 +0200

    another commit

--- src/context.js
+++ src/context.js
@@ -34,7 +34,12 @@ export class Provider extends Component {
     }
 
     componentDidMount(){
-        axios.get('https://cors-anywhere.herokuapp.com/http://jsonplaceholder.typicode.com/users')
+        axios.get('https://cors-anywhere.herokuapp.com/http://jsonplaceholder.typicode.com/users',{
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*' 
+             } 
+        })
             .then(res => this.setState({ contacts:res.data}))
             .catch(err => console.log(err));
     }
