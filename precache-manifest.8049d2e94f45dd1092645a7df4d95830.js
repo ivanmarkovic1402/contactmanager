4c8bca45452f25166cf5476d3237afc4ec050337
ivanmarkovic1402 <ivan.markovic1402@gmail.com>
2020-06-10 11:51:48 +0200

    Updates

---
+++ precache-manifest.8049d2e94f45dd1092645a7df4d95830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c8aa816769ce09c8b8e12030df5b77",
+    "url": "/github.io/contactmanager/index.html"
+  },
+  {
+    "revision": "cc85dbc79d5b81ddda2d",
+    "url": "/github.io/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "446be6ae009f26c98935",
+    "url": "/github.io/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cc85dbc79d5b81ddda2d",
+    "url": "/github.io/contactmanager/static/js/2.a5911414.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github.io/contactmanager/static/js/2.a5911414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446be6ae009f26c98935",
+    "url": "/github.io/contactmanager/static/js/main.df0de45e.chunk.js"
+  },
+  {
+    "revision": "0c0a26d4ac2c07ee9316",
+    "url": "/github.io/contactmanager/static/js/runtime-main.5d18e94b.js"
+  }
+]);
